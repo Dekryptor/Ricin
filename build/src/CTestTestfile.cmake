032a96d2192cd2286324873d1d04f4bbebc6ff2c
SkyzohKey <skyzohkey@protonmail.com>
2016-06-23 07:47:01 +0200

    feat(rewrite): remove build/ folder

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/skyzohkey/Dev/Vala/Ricin/src
-# Build directory: /home/skyzohkey/Dev/Vala/Ricin/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
